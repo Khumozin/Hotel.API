ea6b9c23c322e40ea0de84dce329844976117abc
Khumozin <Khumomogorosi@gmail.com>
2020-08-08 13:38:59 +0200

    Add props to room model

---
+++ Migrations/20200808113050_AddPropsToRoomModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel.API.Migrations
+{
+    public partial class AddPropsToRoomModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Room",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Room",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Room");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Room");
+        }
+    }
+}
