865d2ec80e0c5f85653c67e18bf06a5baaf1257a
Khumozin <Khumomogorosi@gmail.com>
2020-08-08 05:28:54 +0200

    Create system config repository

---
+++ Data/SystemConfigRepo.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Hotel.API.Models;
+
+namespace Hotel.API.Data
+{
+    public class SystemConfigRepo : ISystemConfig
+    {
+        public void CreateSystemConfig(SystemConfig systemConfig)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSystemConfig(SystemConfig systemConfig)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SystemConfig> GetAllSystemConfigs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemConfig GetSystemConfigByID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSystemConfig(SystemConfig systemConfig)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
